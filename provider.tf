5e0da0831bd5d3b4903e10e1099019cc2a72f9f7
akasshdeep9 <102315432+akasshdeep9@users.noreply.github.com>
2022-03-25 22:20:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,11 +6,11 @@
 
 provider "helm" {
   kubernetes {
-    config_paths = [ "./new_cluster-kubeconfig.yml"]
-    #host                   = local.kubeconfig.clusters[0].cluster.server
-    #insecure               = false
-    #client_certificate     = base64decode(local.kubeconfig.users[0].user.client-certificate-data)
-    #client_key             = base64decode(local.kubeconfig.users[0].user.client-key-data)
-    #cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data)
+    #config_paths = [ "./new_cluster-kubeconfig.yml"]
+    host                   = local.kubeconfig.clusters[0].cluster.server
+    insecure               = false
+    client_certificate     = base64decode(local.kubeconfig.users[0].user.client-certificate-data)
+    client_key             = base64decode(local.kubeconfig.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data)
   }
 }
