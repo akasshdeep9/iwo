35b0c5871d903b0609d33c1cf79e68f6a15291c5
akasshdeep9 <102315432+akasshdeep9@users.noreply.github.com>
2022-03-25 12:10:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,8 @@
 #_______________________________________________________________
 
 provider "helm" {
+  enable_tls = false
   kubernetes {
-    enable_tls             = false
     host                   = local.kubeconfig.clusters[0].cluster.server
     client_certificate     = base64decode(local.kubeconfig.users[0].user.client-certificate-data)
     client_key             = base64decode(local.kubeconfig.users[0].user.client-key-data)
